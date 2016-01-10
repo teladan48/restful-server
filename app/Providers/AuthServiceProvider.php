e3169185ffe9a793031510a6c0ca339666cb0389
Yoga Hanggara <yohang88@gmail.com>
2016-01-10 16:29:53 +0700

    access token

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Exception;
 use App\Entities\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -31,10 +33,21 @@ class AuthServiceProvider extends ServiceProvider
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
 
-        Auth::viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+        Auth::viaRequest('api', function (Request $request) {
+
+            $authorization_header = explode(' ', $request->header('Authorization'));
+
+            if (count($authorization_header) != 2 || strpos($authorization_header[0], 'Bearer')) {
+                throw new Exception('Authorization header not set or invalid.');
             }
+
+            $user = User::where('api_token', $authorization_header[1])->first();
+
+            if (is_null($user)) {
+                throw new Exception('Invalid access token.');
+            }
+
+            return $user;
         });
     }
 }
